26bb7542c48f9e5fb2e3cc8d0809e8fb272d48b7
Calinciurariu <calin.ciurariu@gmail.com>
2021-06-21 15:13:19 +0300

    fixed event listeners

--- fe/products.js
+++ fe/products.js
@@ -1,13 +1,17 @@
 
 var mainSearchBar = document.getElementsByClassName("searchTerm");
 var mainSearchButton = document.getElementsByClassName("searchButton");
-mainSearchBar.addEventListener("keyup", function(event) {
+var searchString="oud";
+mainSearchBar[0].addEventListener("keyup", function(event) {
+    searchString = mainSearchBar[0].value ;
     if (event.keyCode === 13) { //13 = enter
       event.preventDefault();
-      document.getElementsByClassName("searchButton").click();
+      mainSearchButton[0].click(searchString);
+
     }
   });
-  mainSearchButton.onclick = function(searchString){
+  mainSearchButton[0].onclick = function(){
+      console.log("search called "+searchString);
       fetch ('http://localhost:8095/be/api/searchPerfumes/'+searchString,{method: 'GET'}).then (response =>{
 
 if (response.status===400 || response.status ===404)
